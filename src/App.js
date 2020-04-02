8577f3486e11b7f95fe96c45bcd1a990414e6394
Simon Vrachliotis <simon@simonswiss.com>
2020-04-02 14:45:00 +1100

    adding data, displayling list of names in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import { names } from "./data";
+
 function App() {
-  return <p>Hello World!</p>;
+  return (
+    <ul>
+      {names.map(entry => (
+        <li className={entry.sex} key={entry.id}>
+          {entry.name}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
